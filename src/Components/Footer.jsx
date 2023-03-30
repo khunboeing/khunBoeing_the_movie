3919d82c3961286a840a6daebbd6fb9c4ad64c3b
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-03-31 04:23:32 +0700

    feat: add target blank on footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
       <a
         className="flex pr-8 cursor-pointer"
         href="https://www.linkedin.com/in/napatpimon-ponpathanapaisarn-162420243/"
+        target="_blank"
       >
         <div className="w-[30px] h-[30px]">
           <img src={icon} />
@@ -16,6 +17,7 @@ const Footer = () => {
       <a
         className="flex cursor-pointer "
         href="https://drive.google.com/drive/u/0/folders/1I5mElksLahhSbT34HVthWn8RnOUXQ2_w?ths=true"
+        target="_blank"
       >
         <div className="w-[30px] h-[30px]">
           <img src={icon} />
